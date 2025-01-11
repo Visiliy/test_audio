3e57f6aedef5d07ce1ed2851ce4c869cfc03bed7
user <irin-shkuratov8@yandex.ru>
2025-01-11 12:28:55 +0300

    /

--- test2.py
+++ test2.py
@@ -1,23 +1,38 @@
-import requests
+from deepgram import (
+    DeepgramClient,
+    PrerecordedOptions,
+    FileSource,
+)
+
 import json
 
-url = "https://api.api2convert.com/v2/jobs"
+AUDIO_FILE = "audioToSave702314.wav"
 
-payload = """{
-  "input": [{
-     "type": "remote",
-   "source": "https://storage.yandexcloud.net/musicbacket/audioToSave751208.mp3"
-  }],
-   "conversion": [{
-   "category": "audio",
-   "target": "opus",
-   "options": {
-       "language_tts": "ru-RU"
-   }
-  }]
-  }"""
-headers = {"x-oc-api-key": "5012110b4afe538504d91639568ecfaa", "Content-Type": "application/json"}
 
-response = requests.request("POST", url, headers=headers, data=payload)
+def main():
+    try:
+        deepgram = DeepgramClient("50a062200dc80b224f63d15175a7b8bb6e10e395")
 
-print(response.text)
+        with open(AUDIO_FILE, "rb") as file:
+            buffer_data = file.read()
+
+        payload: FileSource = {
+            "buffer": buffer_data,
+        }
+
+        options = PrerecordedOptions(
+            model="nova-2",
+            smart_format=True,
+            language="ru",
+        )
+
+        response = deepgram.listen.rest.v("1").transcribe_file(payload, options)
+
+        print(json.loads(response.to_json(indent=4))["results"]["channels"][0]["alternatives"][0]["transcript"])
+
+    except Exception as e:
+        print(f"Exception: {e}")
+
+
+if __name__ == "__main__":
+    main()
